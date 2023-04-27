72ef94d5674e6d0e17b10114c21c38ef42120e92
Tomas <tomasp2266@gmail.com>
2023-04-26 22:35:32 -0300

    ordenados2

--- ordenados.c
+++ ordenados.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include <math.h>
 
 int main(int argc, char *argv[]) {
     int a = atoi(argv[1]);
     int b = atoi(argv[2]);
     int c = atoi(argv[3]);
     bool fuertes = a <= b && b <= c;
-    printf("%s", fuertes ? "ordenado" : "No ordenado");
+    fuertes = fuertes || a >= b && b >= c;
+    printf("%s\n", fuertes ? "ordenado" : "No ordenado");
     return 0;
 }
